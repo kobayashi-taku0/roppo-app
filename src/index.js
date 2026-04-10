a9ab22f7d49ce0633ed07cb454d0c1735dded85c
kobayashi-taku0 <kobayashi.takuo@gmail.com>
2026-04-10 22:46:31 +0900

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import RoppoApp from './RoppoApp';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<RoppoApp />);
